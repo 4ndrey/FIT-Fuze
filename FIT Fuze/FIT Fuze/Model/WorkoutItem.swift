f3087bcaca2d982d8d53add7c35578e7747a6500
Andrei Toropchin <andrei.toropchin@check24.de>
2018-06-08 21:34:52 +0200

    Use class for WorkoutItem instead of struct

--- FIT Fuze/FIT Fuze/Model/WorkoutItem.swift
+++ FIT Fuze/FIT Fuze/Model/WorkoutItem.swift
@@ -6,11 +6,18 @@
 //  Copyright © 2018 FIT. All rights reserved.
 //
 
-struct WorkoutItem {
+class WorkoutItem {
     typealias Id = String
 
     let id: Id
     let exerciseId: Exercise.Id
     var executions: [ExecutionDetails]
-    let nextItem: Any? // WorkoutItem
+    let nextItem: WorkoutItem?
+
+    init(id: Id, exerciseId: Exercise.Id, executions: [ExecutionDetails], nextItem: WorkoutItem? = nil) {
+        self.id = id
+        self.exerciseId = exerciseId
+        self.executions = executions
+        self.nextItem = nextItem
+    }
 }
